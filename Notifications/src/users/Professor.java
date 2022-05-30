65ee4a1a3203a55461a771e2788e055dd3006f59
RawanAlShami <82128109+RawanAlShami@users.noreply.github.com>
2022-05-30 17:26:11 +0200

    Fixed issues assigned to RawanAlShami
    - Course.java : fixed imports users.Professor , users.Student  and users.TA
    - Professor.java: fixed package name to users and parameter name to phDTopic to match naming conventions
    - Student.java : fixed package name to users and parameter name to id to match naming conventions
    - TA.java : fixed package name to users

--- Notifications/src/users/Professor.java
+++ Notifications/src/users/Professor.java
@@ -1,22 +1,28 @@
-package Users;
+package users;
 
-public class TA {
+import java.sql.Date;
+
+public class Professor {
 	String name;
 	String department;
+	Date hirringDate;
+	String phDTopic;
 	String email;
 	String phoneNumber;
 	
 	
-
-	public TA(String name, String department, String email, String phoneNumber) {
+	public Professor(String name, String department, Date hirringDate, String phDTopic, String email,
+			String phoneNumber) {
 		super();
 		this.name = name;
 		this.department = department;
+		this.hirringDate = hirringDate;
+		PhDTopic = phDTopic;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
 	}
-
-
+	
+	
 	public String getEmail() {
 		return email;
 	}
@@ -36,25 +42,38 @@ public class TA {
 		this.phoneNumber = phoneNumber;
 	}
 
+
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getDepartment() {
 		return department;
 	}
-
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+	public Date getHirringDate() {
+		return hirringDate;
+	}
+	public void setHirringDate(Date hirringDate) {
+		this.hirringDate = hirringDate;
+	}
+	public String getPhDTopic() {
+		return PhDTopic;
+	}
+	public void setPhDTopic(String phDTopic) {
+		PhDTopic = phDTopic;
+	}
 	
-	public void notifyTA(String message) {
+	
+	public void notifyProfessor(String message) {
 		// do some stuff
 		
 	}
+	
+	
 	
 }
--- Notifications/src/users/TA.java
+++ Notifications/src/users/TA.java
@@ -1,28 +1,22 @@
-package Users;
+package users;
 
-import java.sql.Date;
-
-public class Professor {
+public class TA {
 	String name;
 	String department;
-	Date hirringDate;
-	String PhDTopic;
 	String email;
 	String phoneNumber;
 	
 	
-	public Professor(String name, String department, Date hirringDate, String phDTopic, String email,
-			String phoneNumber) {
+
+	public TA(String name, String department, String email, String phoneNumber) {
 		super();
 		this.name = name;
 		this.department = department;
-		this.hirringDate = hirringDate;
-		PhDTopic = phDTopic;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
 	}
-	
-	
+
+
 	public String getEmail() {
 		return email;
 	}
@@ -42,38 +36,25 @@ public class Professor {
 		this.phoneNumber = phoneNumber;
 	}
 
-
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-	public Date getHirringDate() {
-		return hirringDate;
-	}
-	public void setHirringDate(Date hirringDate) {
-		this.hirringDate = hirringDate;
-	}
-	public String getPhDTopic() {
-		return PhDTopic;
-	}
-	public void setPhDTopic(String phDTopic) {
-		PhDTopic = phDTopic;
-	}
 	
-	
-	public void notifyProfessor(String message) {
+	public void notifyTA(String message) {
 		// do some stuff
 		
 	}
-	
-	
 	
 }
